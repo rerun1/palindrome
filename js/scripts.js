d9a37239ebc6b4a8c8cd864279cdf77ce80e3040
Suzi Rubino <suzi@suzirubi.com>
2019-08-20 23:58:52 -0700

    start scripting

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,26 @@ $(document).ready(function(){
 
     var userWord = $("input#userWord").val();
 
+    userWord.trim();
+
     console.log(userWord);
+
+//     function reverse(str){
+//   let reversed = "";
+//   for (var i = str.length - 1; i >= 0; i--){
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+
+    for (var i = userWord.length - 1; i >= 0; i --) {
+      var reverse = "";
+      reverse += userWord[i];
+      return reverse;
+    }
+
+    console.log(reverse);
 
   });
 });
